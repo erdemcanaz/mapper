50cc3f5abd27ecc1b0469be24b252cfe9381d266
erdem canaz <erdemcanaz@gmail.com>
2020-01-14 23:55:14 +0300

    +Canvas' ->line

---
+++ app/src/main/java/com/erdem/mapper/CanvasHandler.java
@@ -0,0 +1,45 @@
+package com.erdem.mapper;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+
+public class CanvasHandler extends View {
+    Paint paint;
+    Path path;
+
+    public CanvasHandler(Context context, AttributeSet attrs){
+        super( context, attrs);
+        paint = new Paint();
+        path = new Path();
+        paint.setStrokeWidth(5f);
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+
+
+    }
+    @Override
+    protected  void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        canvas.drawPath(path,paint);
+    }
+    public void cleanCanvas(){
+        path=new Path();
+        invalidate();
+    }
+    public void line(float x1,float y1,float x2,float y2,int STROKE,int COLOR){
+        paint.setStrokeWidth(STROKE);
+        paint.setColor(COLOR);
+
+        path = new Path();
+        path.moveTo(x1, y1);
+        path.lineTo(x2, y2);
+        invalidate();
+    }
+}
--- app/src/main/java/com/erdem/mapper/MainActivity.java
+++ app/src/main/java/com/erdem/mapper/MainActivity.java
@@ -6,17 +6,24 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    CanvasHandler myCanvas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         PermissionHandler.setup_checkPermissions(MainActivity.this,this);
         LocationHandler LOCATION_=new LocationHandler(MainActivity.this);
+
+        myCanvas=new CanvasHandler(this,null);
+        myCanvas.line(0,0,250,250,5, Color.YELLOW);
+        myCanvas.line(300,250,250,300,5, Color.RED);
+        setContentView(myCanvas);
 
     }
     /*PERMISSIONS*/
