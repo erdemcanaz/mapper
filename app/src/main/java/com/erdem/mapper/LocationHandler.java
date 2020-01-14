f5d4d73921ee9b64605fb75c02e9914149664688
erdem canaz <erdemcanaz@gmail.com>
2020-01-14 21:45:14 +0300

    GPS

---
+++ app/src/main/java/com/erdem/mapper/LocationHandler.java
@@ -0,0 +1,36 @@
+package com.erdem.mapper;
+
+import android.app.Activity;
+import android.os.Looper;
+import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+
+public class LocationHandler {
+    private FusedLocationProviderClient client;
+    private LocationRequest request;
+    static double GEO[]={0,0,0};//lan,lon,altitude
+
+    public LocationHandler(Activity activity){
+        client= new FusedLocationProviderClient(activity);
+        request = new LocationRequest();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setFastestInterval(2000);
+        request.setInterval(2500);
+
+        client.requestLocationUpdates(request,new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                GEO[0]=locationResult.getLastLocation().getLatitude();
+                GEO[1]=locationResult.getLastLocation().getLongitude();
+                GEO[2]=locationResult.getLastLocation().getAltitude();
+                Log.d("main","lat:"+LocationHandler.GEO[0]+" lon:"+LocationHandler.GEO[1]+" alt:"+LocationHandler.GEO[2]);
+
+            }
+        }, Looper.getMainLooper());
+    }
+}
---
+++ app/src/main/java/com/erdem/mapper/MainActivity.java
@@ -0,0 +1,52 @@
+package com.erdem.mapper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setup_checkPermissions();
+        LocationHandler LOCATION_=new LocationHandler(MainActivity.this);
+
+    }
+    /*PERMISSIONS*/
+    private String[] PERMISSIONS = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+    };
+    private void setup_checkPermissions(){
+        int PERMISSION_ALL = 1;
+        String[] PERMISSIONS = {
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+        };
+
+        if (!hasPermissions(MainActivity.this, PERMISSIONS)) {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
+        }
+    }
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    /**/
+
+}
